1310cb23eb9929ff31aaacd1b2cb387d374cefa5
Omar <annexomar@gmail.com>
2023-02-21 19:48:37 +0300

    persist gate added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,17 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { store } from "./Redux/store";
+import { store, persistor } from "./Redux/store";
 import { Provider } from "react-redux";
 import { QueryClientProvider, QueryClient } from "react-query";
+import { PersistGate } from "redux-persist/integration/react";
 
 const client = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <QueryClientProvider client={client}>
-        <App />
-      </QueryClientProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <QueryClientProvider client={client}>
+          <App />
+        </QueryClientProvider>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
